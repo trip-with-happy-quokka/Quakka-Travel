b55ce156baa7a8b20aeb122994cf0a23cbb2f4f3
Jay3029 <lttleleepp@gmail.com>
2024-10-21 22:38:54 +0900

    add: initial commit

---
+++ src/main/java/com/sparta/quokkatravel/QuokkaTravelApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.quokkatravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuokkaTravelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuokkaTravelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/quokkatravel/QuokkaTravelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.quokkatravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuokkaTravelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
