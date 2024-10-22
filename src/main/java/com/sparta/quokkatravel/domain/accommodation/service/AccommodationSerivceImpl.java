89759c427b73a7846858136e68dcb63300dd1982
Jay3029 <lttleleepp@gmail.com>
2024-10-22 20:21:16 +0900

    [ADD] Accommodation Repository, Service, Controller 생성

--- src/main/java/com/sparta/quokkatravel/domain/accommodation/service/AccommodationSerivceImpl.java
+++ src/main/java/com/sparta/quokkatravel/domain/accommodation/service/AccommodationSerivceImpl.java
@@ -2,29 +2,43 @@ package com.sparta.quokkatravel.domain.accommodation.service;
 
 import com.sparta.quokkatravel.domain.accommodation.dto.AccommodationRequestDto;
 import com.sparta.quokkatravel.domain.accommodation.dto.AccommodationResponseDto;
+import com.sparta.quokkatravel.domain.accommodation.repository.AccommodationRepository;
+import com.sparta.quokkatravel.domain.accommodation.repository.AccommodationRepositorySupport;
+import com.sparta.quokkatravel.domain.common.dto.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AccommodationSerivceImpl implements AccommodationService {
+
+    private final AccommodationRepository accommodationRepository;
+    private final AccommodationRepositorySupport accommodationRepositorySupport;
+
     @Override
-    public AccommodationResponseDto createAccommodation(AccommodationRequestDto accommodationRequestDto) {
-        return new AccommodationResponseDto();
+    public AccommodationResponseDto createAccommodation(CustomUserDetails customUserDetails, AccommodationRequestDto accommodationRequestDto) {
+
+
+        return null;
     }
 
     @Override
-    public AccommodationResponseDto getAccommodation() {
-        return new AccommodationResponseDto();
+    public AccommodationResponseDto getAllAccommodations(CustomUserDetails customUserDetails) {
+        return null;
     }
 
     @Override
-    public AccommodationResponseDto updateAccommodation(AccommodationRequestDto accommodationRequestDto) {
-        return new AccommodationResponseDto();
+    public AccommodationResponseDto getAccommodation(CustomUserDetails customUserDetails, Long accommodationId) {
+        return null;
     }
 
     @Override
-    public AccommodationResponseDto deleteAccommodation() {
-        return new AccommodationResponseDto();
+    public AccommodationResponseDto updateAccommodation(CustomUserDetails customUserDetails, Long accommodationId, AccommodationRequestDto accommodationRequestDto) {
+        return null;
+    }
+
+    @Override
+    public AccommodationResponseDto deleteAccommodation(CustomUserDetails customUserDetails, Long accommodationId) {
+        return null;
     }
 }
