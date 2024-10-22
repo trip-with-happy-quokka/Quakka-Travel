c7d18e023b6bc91b72062ff0341d427087ae9ce5
banasu0723 <banasu0723@gmail.com>
2024-10-22 21:18:31 +0900

    [FIX] ChatRoom, ChatParticipant entity 수정

--- src/main/java/com/sparta/quokkatravel/domain/chat/entity/ChatRoom.java
+++ src/main/java/com/sparta/quokkatravel/domain/chat/entity/ChatRoom.java
@@ -1,6 +1,7 @@
 package com.sparta.quokkatravel.domain.chat.entity;
 
 import com.sparta.quokkatravel.domain.common.timestamped.Timestamped;
+import com.sparta.quokkatravel.domain.user.entity.User;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -12,21 +13,32 @@ import java.util.List;
 @Entity
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
 public class ChatRoom extends Timestamped {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    // 방 제목
+    @Column(nullable = false)
     private String title;
+
+    // 채팅방을 생성한 방장
+    @ManyToOne
+    @JoinColumn(name = "created_by", nullable = false)
+    private User createdBy;
 
     // 각 방에 여러 참여자 연결될 수 있음
     @OneToMany(mappedBy = "chatRoom", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<ChatParticipant> participants = new ArrayList<>();
+    private List<ChatParticipant> participants;
 
     // 각 방에 여러 메시지 저장될 수 있음
     @OneToMany(mappedBy = "chatRoom", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Chatting> messages = new ArrayList<>();
+    private List<Chatting> messages;
+
+    public ChatRoom(String title, User createdBy) {
+        this.title = title;
+        this.createdBy = createdBy;
+    }
 
 }
