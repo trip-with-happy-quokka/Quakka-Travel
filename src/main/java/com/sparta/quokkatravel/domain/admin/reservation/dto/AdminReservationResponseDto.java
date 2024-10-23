dc8e5a80ed17231dc2d60ea613eb34894e93ea4c
Yehwan An <168810508+davidan94@users.noreply.github.com>
2024-10-23 10:37:05 +0900

    [FIX} AdminReservationDto 수정

--- src/main/java/com/sparta/quokkatravel/domain/admin/reservation/dto/AdminReservationResponseDto.java
+++ src/main/java/com/sparta/quokkatravel/domain/admin/reservation/dto/AdminReservationResponseDto.java
@@ -22,8 +22,8 @@ public class AdminReservationResponseDto {
         this.reservationId = reservation.getId();
         this.userId = reservation.getUser().getId();
         this.accommodationId = reservation.getRoom().getId();
-        this.reservationStatus = reservation.getStatus(); // status 필드 추가 필요
-        this.reservationDate = reservation.getReservationDate(); // reservationDate 필드 추가 필요
+        this.reservationStatus = reservation.getStatus().name(); // Enum을 String으로 변환
+        this.reservationDate = reservation.getReservationDate().atStartOfDay(); // LocalDate를 LocalDateTime으로 변환
         this.totalPrice = BigDecimal.valueOf(reservation.getTotalPrice()); // int를 BigDecimal로 변환
         this.userName = reservation.getUser().getName();  // 관리자가 보는 유저 이름
     }
