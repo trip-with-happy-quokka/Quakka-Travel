d73cf5730059ff1dc7c6ac4422504fa094b733ba
Jay3029 <lttleleepp@gmail.com>
2024-10-23 15:59:46 +0900

    [ADD] Room 구현

---
+++ src/main/java/com/sparta/quokkatravel/domain/room/entity/Room.java
@@ -0,0 +1,56 @@
+package com.sparta.quokkatravel.domain.room.entity;
+
+import com.sparta.quokkatravel.domain.accommodation.entity.Accommodation;
+import com.sparta.quokkatravel.domain.common.timestamped.Timestamped;
+import com.sparta.quokkatravel.domain.room.dto.RoomRequestDto;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Room extends Timestamped {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String description;
+
+    @Column
+    private int capacity;
+
+    @Column
+    private int pricePerOverCapacity;
+
+    @Column
+    private int pricePerNight;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "accommodation_id")
+    private Accommodation accommodation;
+
+
+    public Room(RoomRequestDto roomRequestDto) {
+        this.name = roomRequestDto.getName();
+        this.description = roomRequestDto.getDescription();
+        this.capacity = roomRequestDto.getCapacity();
+        this.pricePerOverCapacity = roomRequestDto.getPricePerOverCapacity();
+        this.pricePerNight = roomRequestDto.getPricePerNight();
+    }
+
+    public void update(String name, String description, int capacity, int pricePerOverCapacity, int pricePerNight) {
+        this.name = name;
+        this.description = description;
+        this.capacity = capacity;
+        this.pricePerOverCapacity = pricePerOverCapacity;
+        this.pricePerNight = pricePerNight;
+    }
+}
