f607cffb39c28a6d51ad9534c033d6885d04273b
banasu0723 <banasu0723@gmail.com>
2024-10-28 16:38:39 +0900

    [ADD] Actuator 및 프로메테우스 의존성 추가

--- src/main/java/com/sparta/quokkatravel/domain/common/config/SecurityConfig.java
+++ src/main/java/com/sparta/quokkatravel/domain/common/config/SecurityConfig.java
@@ -42,6 +42,7 @@ public class SecurityConfig {
                         .requestMatchers("/ws/**").permitAll()
                         .requestMatchers("/users/**").permitAll()
                         .requestMatchers("/api/v1/guest/**").permitAll()
+                        .requestMatchers("/actuator/**").permitAll()
                         .anyRequest().authenticated() // 그 외의 모든 요청은 인증 필요
                 )
                 .formLogin(form -> form // 로그인 성공 핸들러 추가
