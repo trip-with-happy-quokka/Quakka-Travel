0ad92ced9ce404a61ab24877de41384bd3504af1
banasu0723 <banasu0723@gmail.com>
2024-10-28 17:27:31 +0900

    [FIX] WebConfig 및 WebSocketConfig 수정

--- src/main/java/com/sparta/quokkatravel/domain/common/config/WebConfig.java
+++ src/main/java/com/sparta/quokkatravel/domain/common/config/WebConfig.java
@@ -9,8 +9,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/ws/**") // WebSocket 엔드포인트에 대한 CORS 설정
-                .allowedOrigins("http://localhost:3000") // 허용할 출처
+                //.allowedOrigins("http://localhost:3000") // 허용할 출처
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
-                .allowCredentials(true); // 자격 증명 포함 여부
+                .allowCredentials(true) // 자격 증명 포함 여부
+                .allowedOriginPatterns("*");
     }
 }
--- src/main/java/com/sparta/quokkatravel/domain/common/config/WebSocketConfig.java
+++ src/main/java/com/sparta/quokkatravel/domain/common/config/WebSocketConfig.java
@@ -13,17 +13,17 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     // 메시지 브로커 설정: 클라이언트가 /app으로 시작하는 메시지를 서버로 전송하고, /topic으로 브로드캐스트 가능하게 설정
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/topic"); // /top 으로 시작하는 stomp 메세지는 브로커로 라우팅함
-        registry.setApplicationDestinationPrefixes("/app"); // /app 으로 시작하는 stomp 메세지의 경로는 @controller @MessageMaping 메서드로 라우팅
+        registry.enableSimpleBroker("/topic"); //해당 주소를 구독하고 있는 클라이언트들에게 메세지 전달
+        registry.setApplicationDestinationPrefixes("/app"); //클라이언트에서 보낸 메세지를 받을 prefix
     }
 
     // STOMP 엔드포인트 설정: 웹소켓 연결을 위한 엔드포인트로 /ws-chat을 정의, SockJS 사용을 허용
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         // 클라이언트가 연결할 WebSocket 엔드포인트를 정의
-        registry.addEndpoint("/ws-chat")
+        registry.addEndpoint("/ws-chat") //SockJS 연결 주소
                 .setAllowedOriginPatterns("*") // CORS 허용 범위
-                .withSockJS(); // 가능한 경로 설정 ( 전체 오픈 : 기호에따라 수정 )
+                .withSockJS(); //버전 낮은 브라우저에서도 적용 가능
     }
 
 }
