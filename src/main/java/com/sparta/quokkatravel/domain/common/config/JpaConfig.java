5f6356edb9f99dfabbfc189c67995e86ff215f93
Yehwan An <168810508+davidan94@users.noreply.github.com>
2024-11-01 00:42:14 +0900

    [FIX] JpaConfig 수정

--- src/main/java/com/sparta/quokkatravel/domain/common/config/JpaConfig.java
+++ src/main/java/com/sparta/quokkatravel/domain/common/config/JpaConfig.java
@@ -1,6 +1,9 @@
 package com.sparta.quokkatravel.domain.common.config;
 
 import javax.sql.DataSource;
+
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateProperties;
+import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -8,19 +11,32 @@ import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Configuration
-@EnableJpaRepositories(basePackages = "com.sparta.quokkatravel.domain")  // JPA 레포지토리 경로
+@EnableJpaRepositories(basePackages = "com.sparta.quokkatravel.domain")
 public class JpaConfig {
+
+    private final JpaProperties jpaProperties;
+
+    public JpaConfig(JpaProperties jpaProperties) {
+        this.jpaProperties = jpaProperties;
+    }
 
     @Bean
     @Primary
     public LocalContainerEntityManagerFactoryBean entityManagerFactory(
             EntityManagerFactoryBuilder builder,
             DataSource dataSource) {
+
+        Map<String, Object> properties = new HashMap<>(jpaProperties.getProperties());
+
         return builder
                 .dataSource(dataSource)
                 .packages("com.sparta.quokkatravel.domain")  // JPA 엔티티 패키지 경로
                 .persistenceUnit("default")
+                .properties(properties)
                 .build();
     }
 }
