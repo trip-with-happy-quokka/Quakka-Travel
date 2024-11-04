7a926a863b0e388abe7f693b7f84d6360ed2a75e
안동환 <ddh1713flrwls1713@gmail.com>
2024-11-04 20:56:18 +0900

    [FIX] HostAccommodationController 변경
    1. 주석 내용 변경

--- src/main/java/com/sparta/quokkatravel/domain/accommodation/controller/HostAccommodationController.java
+++ src/main/java/com/sparta/quokkatravel/domain/accommodation/controller/HostAccommodationController.java
@@ -41,7 +41,7 @@ public class HostAccommodationController {
         return ResponseEntity.ok(ApiResponse.created("숙소 생성 성공", hostAccommodationResponseDto));
     }
 
-    // 숙소 전체 조회
+    // 내 숙소 전체 조회
     @GetMapping("/accommodations")
     @Operation(summary = "숙소 전체 조회", description = "HOST 유저의 숙소들을 조회하는 API")
     public ResponseEntity<?> getAllAccommodation(@AuthenticationPrincipal CustomUserDetails customUserDetails,
