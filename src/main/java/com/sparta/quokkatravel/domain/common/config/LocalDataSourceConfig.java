c4984a8b8be2ed1e9dbfcd85e1ba952f7a091144
vege4944 <vege4944@gmail.com>
2024-11-20 14:04:07 +0900

    batch 최신화

--- src/main/java/com/sparta/quokkatravel/domain/common/config/LocalDataSourceConfig.java
+++ src/main/java/com/sparta/quokkatravel/domain/common/config/LocalDataSourceConfig.java
@@ -1,37 +1,37 @@
-//package com.sparta.quokkatravel.domain.common.config;
-//
-//import com.zaxxer.hikari.HikariDataSource;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
-//import org.springframework.boot.context.properties.ConfigurationProperties;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.context.annotation.Primary;
-//import org.springframework.context.annotation.Profile;
-//import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
-//
-//import javax.sql.DataSource;
-//
-//@Profile("local")
-//@Slf4j
-//@Configuration
-//public class LocalDataSourceConfig {
-//
-//    @Bean
-//    @Primary
-//    @ConfigurationProperties(prefix = "spring.datasource")
-//    public DataSourceProperties quokkaDataSourceProperties() {
-//        log.info("Configuring quokka_travel datasource properties");
-//        return new DataSourceProperties();
-//    }
-//
-//    @Primary
-//    @Bean(name = "dataSource")
-//    public DataSource quokkaDataSource() {
-//        return new LazyConnectionDataSourceProxy(
-//                quokkaDataSourceProperties().initializeDataSourceBuilder()
-//                        .type(HikariDataSource.class).build()
-//        );
-//    }
-//
-//}
+package com.sparta.quokkatravel.domain.common.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
+
+import javax.sql.DataSource;
+
+@Profile("local")
+@Slf4j
+@Configuration
+public class LocalDataSourceConfig {
+
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSourceProperties quokkaDataSourceProperties() {
+        log.info("Configuring quokka_travel datasource properties");
+        return new DataSourceProperties();
+    }
+
+    @Primary
+    @Bean(name = "dataSource")
+    public DataSource quokkaDataSource() {
+        return new LazyConnectionDataSourceProxy(
+                quokkaDataSourceProperties().initializeDataSourceBuilder()
+                        .type(HikariDataSource.class).build()
+        );
+    }
+
+}
